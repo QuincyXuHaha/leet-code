a4c85ce2608e4a4fa0cb600e81b603ef3fa7837e
quincy <quincy.xu@qq.com>
2019-01-23 15:03:55 +0800

    字符串转为int数字

--- src/main/java/com/quincy/leetcode/easy/string/Str.java
+++ src/main/java/com/quincy/leetcode/easy/string/Str.java
@@ -189,10 +189,174 @@ public class Str {
         return true;
     }
 
+    /**
+     * <a href='https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/5/strings/37/'></a>
+     * <p>
+     * 请你来实现一个 atoi 函数，使其能将字符串转换成整数。
+     * <p>
+     * 首先，该函数会根据需要丢弃无用的开头空格字符，直到寻找到第一个非空格的字符为止。
+     * <p>
+     * 当我们寻找到的第一个非空字符为正或者负号时，则将该符号与之后面尽可能多的连续数字组合起来，作为该整数的正负号；假如第一个非空字符是数字，则直接将其与之后连续的数字字符组合起来，形成整数。
+     * <p>
+     * 该字符串除了有效的整数部分之后也可能会存在多余的字符，这些字符可以被忽略，它们对于函数不应该造成影响。
+     * <p>
+     * 注意：假如该字符串中的第一个非空格字符不是一个有效整数字符、字符串为空或字符串仅包含空白字符时，则你的函数不需要进行转换。
+     * <p>
+     * 在任何情况下，若函数不能进行有效的转换时，请返回 0。
+     * <p>
+     * 说明：
+     * <p>
+     * 假设我们的环境只能存储 32 位大小的有符号整数，那么其数值范围为 [−231,  231 − 1]。如果数值超过这个范围，qing返回  INT_MAX (231 − 1) 或 INT_MIN (−231) 。
+     * <p>
+     * 示例 1:
+     * <p>
+     * 输入: "42"
+     * 输出: 42
+     * 示例 2:
+     * <p>
+     * 输入: "   -42"
+     * 输出: -42
+     * 解释: 第一个非空白字符为 '-', 它是一个负号。
+     * 我们尽可能将负号与后面所有连续出现的数字组合起来，最后得到 -42 。
+     * 示例 3:
+     * <p>
+     * 输入: "4193 with words"
+     * 输出: 4193
+     * 解释: 转换截止于数字 '3' ，因为它的下一个字符不为数字。
+     * 示例 4:
+     * <p>
+     * 输入: "words and 987"
+     * 输出: 0
+     * 解释: 第一个非空字符是 'w', 但它不是数字或正、负号。
+     * 因此无法执行有效的转换。
+     * 示例 5:
+     * <p>
+     * 输入: "-91283472332"
+     * 输出: -2147483648
+     * 解释: 数字 "-91283472332" 超过 32 位有符号整数范围。
+     * 因此返回 INT_MIN (−231) 。
+     * <p>
+     * 思路：无非就是按照正常思维来
+     * 1、字符串判空，去掉前面的空格
+     * 2、看看第一个字符是数字还是正负号，然后得到第一个数字的下标
+     * 3、数字字符转换为数字，要注意溢出的情况
+     *
+     * @param str
+     * @return
+     */
+    public static int myAtoi(String str) {
+//        if (str == null || "".equals(str)) {
+//            return 0;
+//        }
+//
+//        int start = 0;
+//        int len = str.length();
+//        // 先去除前面的空格
+//        while (start < len) {
+//            if (str.charAt(start) != ' ') {
+//                break;
+//            }
+//            start++;
+//        }
+//        boolean isPositive = true;
+//        // 判断第一个字符串是啥
+//        if (str.charAt(start) == '-') {
+//            start++;
+//            isPositive = false;
+//        } else if (str.charAt(start) == '+') {
+//            start++;
+//        } else if (str.charAt(start) > '9' || str.charAt(start) < '0') {
+//            return 0;
+//        }
+//
+//        // 抽取出正确的字符串数字
+//        int end = start;
+//        while (end < len) {
+//            if (str.charAt(end) > '9' || str.charAt(end) < '0') {
+//                break;
+//            }
+//            end++;
+//        }
+//        int ret = Integer.parseInt(str.substring(start, end));// 会溢出，见下面的方法
+//        if (!isPositive && ret == Integer.MAX_VALUE) {
+//            return 0;
+//        }
+//        return isPositive ? ret : -ret;
+
+//        int index = 0, sign = 1, total = 0;
+//        //1. Empty string
+//        if (str == null || str.length() == 0) {
+//            return 0;
+//        }
+//
+//        //2. Remove Spaces
+//        while (index < str.length() && str.charAt(index) == ' ') {
+//            index++;
+//        }
+//
+//        //3. Handle signs
+//        if (index < str.length() && str.charAt(index) == '+' || str.charAt(index) == '-') {
+//            sign = str.charAt(index) == '+' ? 1 : -1;
+//            index++;
+//        }
+//
+//        //4. Convert number and avoid overflow
+//        while (index < str.length()) {
+//            int digit = str.charAt(index) - '0';
+//            if (digit < 0 || digit > 9) {
+//                break;
+//            }
+//
+//            //check if total will be overflow after 10 times and add digit
+//            if (Integer.MAX_VALUE / 10 <= total && Integer.MAX_VALUE % 10 < digit) {
+//                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+//            }
+//            total = 10 * total + digit;
+//            index++;
+//        }
+//        return total * sign;
+        if (str == null || str.length() == 0) {
+            return 0;
+        }
+        str = str.trim();
+        if ("".equals(str)) {
+            return 0;
+        }
+        char firstChar = str.charAt(0);
+        int sign = 1, start = 0, len = str.length();
+        long sum = 0;
+        if (firstChar == '+') {
+            start++;
+        } else if (firstChar == '-') {
+            sign = -1;
+            start++;
+        }
+        for (int i = start; i < len; i++) {
+            if (!Character.isDigit(str.charAt(i))) {
+                return (int) sum * sign;
+            }
+            sum = sum * 10 + str.charAt(i) - '0';
+            if (sign == 1 && sum > Integer.MAX_VALUE) {
+                return Integer.MAX_VALUE;
+            }
+            if (sign == -1 && (-1) * sum < Integer.MIN_VALUE) {
+                return Integer.MIN_VALUE;
+            }
+        }
+        return (int) sum * sign;
+    }
+
+
     public static void main(String[] args) {
         isAnagram("a", "b");
         System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
         System.out.println(isPalindrome("race a car"));
+        System.out.println(myAtoi("  42"));
+        System.out.println(myAtoi(" -42"));
+        System.out.println(myAtoi(" words and 987"));
+        System.out.println(myAtoi(" 4193 with words"));
+        System.out.println(myAtoi(" -91283472332"));
+
     }
 
 }
